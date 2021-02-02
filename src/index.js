22d403694fd61f6ca4fa59f6528bc3a7080c4bd7
Lultik <mihwil27021@gmail.com>
2021-02-02 21:15:30 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,33 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  let char, last, stack = [];
-  let brackets = {};
+    let stack = [];
+    let brackets = {};
 
-  if (str.length % 2 !== 0) return false;
+    if (str.length % 2 !== 0) return false;
 
-  bracketsConfig.flat().map((item, index, array) => {
-    if ( index % 2 !== 0 ) {
-        brackets[array[index - 1]] = item;
-    }
-  });
-
-  for(let i = 0; i < str.length; i++) {
-    char = str[i];
-
-    for (let key in brackets) {
-      if(char === key && !stack.includes(brackets[key])){
-        stack.push(char);
-      } else if (char === brackets[key]) {
-
-        if(stack.length > 0){
-          last = stack[stack.length - 1];
-
-          if ((char === brackets[key] && last === key)){
-            stack.pop();
-          }
+    bracketsConfig.flat().map((item, index, array) => {
+        if ( index % 2 !== 0 ) {
+            brackets[array[index - 1]] = item;
         }
-      }
-    }
-  }
+    });
 
-  return stack.length === 0;
-};
+    for (let char of str) {
+        for (let key in brackets){
+            if (char === key && !stack.includes(brackets[key])) {
+                stack.push(char);
+            } else if (char === brackets[key] && stack[stack.length - 1] === key) {
+               stack.pop();
+            }
+        }
+    }
+
+    return stack.length === 0;
+}
---
+++ src/index_2020Q1.js
@@ -0,0 +1,32 @@
+module.exports = function check(str, bracketsConfig) {
+  let char, last, stack = [];
+  let brackets = {};
+
+  if (str.length % 2 !== 0) return false;
+
+  bracketsConfig.flat().map((item, index, array) => {
+    if ( index % 2 !== 0 ) {
+        brackets[array[index - 1]] = item;
+    }
+  });
+
+  for(let i = 0; i < str.length; i++) {
+    char = str[i];
+
+    for (let key in brackets) {
+      if(char === key && !stack.includes(brackets[key])){
+        stack.push(char);
+      } else if (char === brackets[key]) {
+
+        if(stack.length > 0){
+          last = stack[stack.length - 1];
+
+          if ((char === brackets[key] && last === key)){
+            stack.pop();
+          }
+        }
+      }
+    }
+  }
+  return stack.length === 0;
+};
